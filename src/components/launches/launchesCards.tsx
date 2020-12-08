a3db06425dd06219b25c4b45cb09bdc1819c787a
alayraza <50444174+alayraza@users.noreply.github.com>
2020-12-08 15:59:15 +0500

    changes

--- src/components/launches/launchesCards.tsx
+++ src/components/launches/launchesCards.tsx
@@ -142,32 +142,6 @@ const LaunchesCards = () => {
                         target="_blank"
                         rel="noopener noreferrer"
                       >
-                        <span className="rounded-full w-12 h-12 primarybutton flex justify-center items-center cursor-pointer transform scale-100 hover:scale-110 bounce transition duration-300 ease-in-out">
-                          <svg
-                            id="Capa_1"
-                            x="0px"
-                            y="0px"
-                            viewBox="0 0 448.011 448.011"
-                            width="20px"
-                            height="20px"
-                            className="hovered-paths"
-                            style={{ marginLeft: "4px" }}
-                          >
-                            <g>
-                              <g>
-                                <g>
-                                  <path
-                                    d="M438.731,209.463l-416-192c-6.624-3.008-14.528-1.216-19.136,4.48c-4.64,5.696-4.8,13.792-0.384,19.648l136.8,182.4    l-136.8,182.4c-4.416,5.856-4.256,13.984,0.352,19.648c3.104,3.872,7.744,5.952,12.448,5.952c2.272,0,4.544-0.48,6.688-1.472    l416-192c5.696-2.624,9.312-8.288,9.312-14.528S444.395,212.087,438.731,209.463z"
-                                    data-original="#000000"
-                                    className="hovered-path active-path"
-                                    data-old_color="#000000"
-                                    fill="#FFFFFF"
-                                  />
-                                </g>
-                              </g>
-                            </g>{" "}
-                          </svg>
-                        </span>
                       </a>
                     </div>
                   </div>
